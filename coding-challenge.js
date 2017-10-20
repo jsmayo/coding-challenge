4f68ac7ec9cf5d1a7006f39e11614e51f7391e0c
Steven Mayo <jsmayo91@gmail.com>
2017-10-20 01:31:55 -0400

    Adds: Solution to problems 2 and 3.   * To answer each problem specifically, I did not reuse code from 	solution 2 as part of the solution for problem 3. If optimizing 	performance/speed was needed, I would avoid repeating code.

--- coding-challenge.js
+++ coding-challenge.js
@@ -1,7 +1,36 @@
+const bands = ['Kiss', 'Aerosmith', 'ACDC', 'Led Zeppelin', 'Nickelback'];
 
-
+/** SOLUTION TO PROBLEM 1:
+  * This function will output an alert to the user when invoked.
+  */
 function whosAwesome() {
   alert("Durham is awesome!");
 }
+// whosAwesome(); Example of invoking whosAwesome()
 
-whosAwesome();
+/** SOLUTION TO PROBLEM 2:
+  * This function will loop through the given 'bands' array and print an alert
+  * of 'I love [bandName]' for each element within the 'bands' array.
+  */
+  function musicIsLife() {
+    for(var i = 0; i < bands.length; i++) {
+      alert("I love " + bands[i]);
+    }
+  }
+  // musicIsLife(); Example call for invoking the musicIsLife() function.
+
+  /** SOLUTION TO PROBLEM 3: 
+    * This function will loop through the 'bands' array and print out an alert 
+    * of ' I love [bandName]' for each band element except 'Nickelback'.
+    * The alert for 'Nickelback' is altered to say "I DON'T love Nickelback!"
+    */
+    function noNickelback() {
+      for(var i = 0; i < bands.length; i++) {
+        if(bands[i] === "Nickelback") alert("I DON'T love Nickelback!");
+        else alert("I love " + bands[i]);
+      }
+    }
+    // noNickelback() Example of invoking noNickelback() function. 
+    /* I would have reused code from the musicIsLife() function, but I wanted
+       to have my solutions individually blocked out for each problem.
+     */
